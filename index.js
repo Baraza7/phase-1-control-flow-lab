5ec8840b681493e92852ec49ecbe166f8afca868
baraza7 <barazabrian87@gmail.com>
2022-12-05 21:04:12 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+let feet = 199;
+
+function scuberGreetingForFeet(feet){
+if(feet <= 400){
+  return "This one is on me!";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+else if (feet > 2000 && feet <= 2500){
+  return "I will gladly take your thirty bucks.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+else if (feet > 2500){
+  return "No can do.";
 }
+}
+
+// let city = NYC;
+function ternaryCheckCity(city){
+  //return city === NYC? 'Ok, Sounds good.':'No go.'; (not working!?)
+  //city = NYC? ('Ok, Sounds good.') : ('No go.'); (not working!?)
+  if(city === 'NYC') {
+    return 'Ok, sounds good.'
+  } else {
+    return 'No go.'
+}
+}
+
+
+function switchOnCharmFromTip(tip){
+switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+    return 'Thank you.';
+    break;
+    default:
+      return 'Bye.';
+      break;
+}
+}
+
